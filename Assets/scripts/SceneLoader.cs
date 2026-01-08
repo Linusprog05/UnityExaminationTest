6bb8eaee8b5339737c5ba857b9e2571eb2ddd8f4
Linusprog05 <c25linsu@student.his.se>
2026-01-08 12:20:54 +0100

    added player with collision

---
+++ Assets/scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 1);
+    }
+    public void LoadSecondScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex + 2);
+    }
+}
